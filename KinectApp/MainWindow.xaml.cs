01275c393efa06b35c96040d8ac29006422f79f7
Matthew Afable <lbafable@up.edu.ph>
2019-03-28 18:46:29 +0800

    Initial commit

---
+++ KinectApp/MainWindow.xaml.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using Microsoft.Kinect;
+
+namespace KinectApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Mode mode = Mode.Color;
+
+        KinectSensor sensor;
+        MultiSourceFrameReader reader;
+        IList<Body> bodies;
+
+        bool displayBody = false;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            sensor = KinectSensor.GetDefault();
+            if (sensor != null)
+            {
+                sensor.Open();
+
+                reader = sensor.OpenMultiSourceFrameReader(FrameSourceTypes.Color |
+                                                           FrameSourceTypes.Depth |
+                                                           FrameSourceTypes.Infrared |
+                                                           FrameSourceTypes.Body);
+                reader.MultiSourceFrameArrived += Reader_MultiSourceFrameArrived;
+            }
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            if (reader != null)
+            {
+                reader.Dispose();
+            }
+
+            if (sensor != null)
+            {
+                sensor.Close();
+            }
+        }
+
+        private void Reader_MultiSourceFrameArrived(object sender, MultiSourceFrameArrivedEventArgs e)
+        {
+            var reference = e.FrameReference.AcquireFrame();
+
+            using (var frame = reference.ColorFrameReference.AcquireFrame())
+            {
+                if (frame != null)
+                {
+                    if (mode == Mode.Color)
+                    {
+                        camera.Source = frame.ToBitMap();
+                        r_camera.Source = frame.ToBitMap();
+                    }
+                }
+            }
+
+            using (var frame = reference.DepthFrameReference.AcquireFrame())
+            {
+                if (frame != null)
+                {
+                    if (mode == Mode.Depth)
+                    {
+                        camera.Source = frame.ToBitmap();
+                        r_camera.Source = frame.ToBitmap();
+                    }
+                }
+            }
+
+            using (var frame = reference.InfraredFrameReference.AcquireFrame())
+            {
+                if (frame != null)
+                {
+                    if (mode == Mode.Infrared)
+                    {
+                        camera.Source = frame.ToBitmap();
+                        r_camera.Source = frame.ToBitmap();
+                    }
+                }
+            }
+
+            using (var frame = reference.BodyFrameReference.AcquireFrame())
+            {
+                if (frame != null)
+                {
+                    canvas.Children.Clear();
+                    r_canvas.Children.Clear();
+
+                    bodies = new Body[frame.BodyFrameSource.BodyCount];
+
+                    frame.GetAndRefreshBodyData(bodies);
+
+                    foreach (var body in bodies)
+                    {
+                        if (body != null)
+                        {
+                            if (body.IsTracked)
+                            {
+                                if (displayBody)
+                                {
+                                    canvas.DrawSkeleton(body);
+                                    r_canvas.DrawSkeleton(body);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void Color_Click(object sender, RoutedEventArgs e)
+        {
+            mode = Mode.Color;
+        }
+
+        private void Depth_Click(object sender, RoutedEventArgs e)
+        {
+            mode = Mode.Depth;
+        }
+
+        private void Infrared_Click(object sender, RoutedEventArgs e)
+        {
+            mode = Mode.Infrared;
+        }
+
+        private void Body_Click(object sender, RoutedEventArgs e)
+        {
+            displayBody = !displayBody;
+        }
+    }
+
+    public enum Mode
+    {
+        Color,
+        Depth,
+        Infrared
+    }
+}
